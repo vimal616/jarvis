ed4c1be65f858ab3f65b02321a4ba8d5b4f49e6a
vimalviswambharan <vimalviswambharan@jarvis>
2019-09-23 15:41:32 +0530

    second commit

--- Main/src/com/faith/test/Main.java
+++ Main/src/com/faith/test/Main.java
@@ -5,14 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SalesData objSalesData=new SalesData();
-		System.out.println("hello ust ripo");
+		System.out.println("hello ust man ripo");
 		displayGreetings();
 		objSalesData.display();
 		
 
 	}
 	private static void displayGreetings(){
-		System.out.println("welcome to sales");
+		System.out.println("welcome to gsales");
 	}
 
 }
