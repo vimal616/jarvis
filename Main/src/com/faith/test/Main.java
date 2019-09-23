3ae514ba66ebdd04238010cb76e45a6d5828a35a
vimalviswambharan <vimalviswambharan@jarvis>
2019-09-23 16:34:48 +0530

    sixth commit

--- Main/src/com/faith/test/Main.java
+++ Main/src/com/faith/test/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 	}
 	private static void displayGreetings(){
-		System.out.println("welcome to javass peopledfs");
+		System.out.println("welcome to tje java tutorial peopledfs");
 	}
 
 }
