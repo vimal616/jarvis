377831630eb1e554e2ef49bd4b83173ce30a039a
vimalviswambharan <vimalviswambharan@jarvis>
2019-09-23 15:16:19 +0530

    initial commit

---
+++ Main/src/com/faith/test/Main.java
@@ -0,0 +1,18 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData objSalesData=new SalesData();
+		System.out.println("hello ust ripo");
+		displayGreetings();
+		objSalesData.display();
+		
+
+	}
+	private static void displayGreetings(){
+		System.out.println("welcome to sales");
+	}
+
+}
